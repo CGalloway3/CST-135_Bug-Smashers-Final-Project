337294308ca67cdc0b8b7d649239d19786f7dd52
DESKTOP-A6JNF32\Office Computer <CGalloway3@my.gcu.edu>
2018-04-01 18:25:02 -0500

    Altered Dispenser class to include default constructor actions in createProducts for candy, chips, and gum

--- Joint submission/Topic 2/Vending Machine Java Classes/Dispenser.java
+++ Joint submission/Topic 2/Vending Machine Java Classes/Dispenser.java
@@ -14,7 +14,7 @@ public class Dispenser {
 	
     // Private fields
     
-	private final ArrayList<Product> productlist = new ArrayList<>();
+	private final ArrayList<Product> productList = new ArrayList<>();
 	
     // Public constructors
         
@@ -26,53 +26,65 @@ public class Dispenser {
         
 	private void createProducts() {              //creates products
 		
-		// Create an empty product to test default constructor.
+		// Create an empty drink product to test default constructor.
                 Product empty = new Drink();
-                productlist.add(empty);
+                productList.add(empty);
             
                 
                 // Create a drink product to test overloaded constructor with parameter list.
                 Drink pepsi = new Drink("Pepsi", "A1", 10, 1.75);
-		productlist.add(pepsi);
+		productList.add(pepsi);
 		
 		// Create a drink product to test copy constructor and renaming functionality.
                 Drink dietpepsi = new Drink(pepsi);
 		dietpepsi.renameProduct("Diet Pepsi");
 		dietpepsi.relocateProduct("A2");
-		productlist.add(dietpepsi);
+		productList.add(dietpepsi);
 		
+                
+		// Create an empty chips product to test default constructor.
+                empty = new Chips();
+                productList.add(empty);
                 
                 // Create a chip product to test overloaded constructor with parameter list.
 		Chips ruffles = new Chips("Ruffles", "B1", 5, 1.00);
-		productlist.add(ruffles);
+		productList.add(ruffles);
 		
 		// Create a chip product to test copy constructor and renaming functionality.
 		Chips doritos = new Chips(ruffles);
 		doritos.renameProduct("Doritos");
 		doritos.relocateProduct("B2");
-		productlist.add(doritos);
+		productList.add(doritos);
 		
 		
+		// Create an empty candy procust to test default constructor.
+                empty = new Candy();
+                productList.add(empty);
+                
                 // Create a candy product to test overloaded constructor with parameter list.
 		Candy kitkat = new Candy("Kit Kat", "C1", 12, .75);
-		productlist.add(kitkat);
+		productList.add(kitkat);
 		
 		// Create a candy product to test copy constructor and renaming functionality.
 		Candy snickers = new Candy(kitkat);
 		snickers.renameProduct("Snickers");
 		snickers.relocateProduct("C2");
-		productlist.add(snickers);
+		productList.add(snickers);
 		
 		
-                // Create a gum product to test overloaded constructor with parameter list.
+ 		// Create an empty gum product to test default constructor.
+                empty = new Gum();
+                productList.add(empty);
+                
+               // Create a gum product to test overloaded constructor with parameter list.
 		Gum orbitpep = new Gum("Orbit Peppermint", "D1", 15, .50);
-		productlist.add(orbitpep);
+		productList.add(orbitpep);
 		
 		// Create a gum product to test copy constructor and renaming functionality.
 		Gum orbitspear = new Gum(orbitpep);
 		orbitspear.renameProduct("Orbit Spearmint");
 		orbitspear.relocateProduct("D2");
-		productlist.add(orbitspear);
+		productList.add(orbitspear);
 	}
 	
     // Additional public methods
@@ -81,9 +93,9 @@ public class Dispenser {
 		
 		int counter = 0;
 		
-		while (counter < productlist.size()) {
+		while (counter < productList.size()) {
 			
-			System.out.println(productlist.get(counter).toString());
+			System.out.println(productList.get(counter).toString());
 			
 			counter++;
 			
