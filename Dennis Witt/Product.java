e74991ee2aa7b8d71e4c54789695c7b2fa86dc86
wittscorpios <d.witt@hotmail.com>
2018-04-08 22:32:42 -0400

    Create Product.java

---
+++ Dennis Witt/Product.java
@@ -0,0 +1,83 @@
+package vendingmachine;
+
+public abstract class Product implements IVendingMachine {
+
+    private String productName;
+    private String productLocation;
+    private int productQuantity;
+    private double productPrice;
+
+    public Product() {	//no argument constructor
+    }
+
+    public Product(String name, String location, int quantity, double price) {   //overloaded constructor with parameters
+        productName = name;
+        productLocation = location;
+        productQuantity = quantity;
+        productPrice = price;
+    }
+
+    public Product(Product copy) {    //overloaded constructor that copies another objects fields
+        productName = copy.getName();
+        productLocation = copy.getLocation();
+        productQuantity = copy.getQuantity();
+        productPrice = copy.getPrice();
+    }
+
+
+    public void restockProduct(int newQuantity) {     //restock and reprice methods
+        this.productQuantity = newQuantity;
+    }
+
+    public void repriceProduct(double newPrice) {
+        this.productPrice = newPrice;
+    }
+
+    public void setName(String newName) {     //setters and getters for copying and admin options
+        this.productName = newName;
+    }
+
+    public String getName() {
+        return this.productName;
+    }
+
+    public void setLocation(String newLocation) {
+        this.productLocation = newLocation;
+    }
+
+    public String getLocation() {
+        return this.productLocation;
+    }
+
+    public int getQuantity() {
+        return this.productQuantity;
+    }
+
+    public double getPrice() {
+        return this.productPrice;
+    }
+
+    public String toString() {     //toString method
+        return productName + ", " + productLocation + ", " + productQuantity + ", " + productPrice;
+    }
+
+    @Override
+    public String productName() {
+        return null;
+    }
+
+    @Override
+    public String productLocation() {
+        return null;
+    }
+
+    @Override
+    public int productQuantity() {
+        return 0;
+    }
+
+    @Override
+    public double productPrice() {
+        return 0;
+    }
+}
