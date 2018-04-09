7bd25fe86d1131ece7ee0624b6bb562438aeced2
DESKTOP-A6JNF32\Office Computer <CGalloway3@my.gcu.edu>
2018-04-09 00:43:30 -0500

    Added user interface in main

--- Chad Galloway/Topic 3/Main.java
+++ Chad Galloway/Topic 3/Main.java
@@ -8,12 +8,46 @@
 
 package vendingmachine;
 
+import java.util.Scanner;
+
 class Main {
 
 	public static void main(String[] args) {
 		
 		Dispenser main = new Dispenser();
 		main.displayProducts();
+                
+                System.out.println("To select a product please type its name. To enter administration mode type \"admin\"");
+                Scanner input = new Scanner(System.in);
+                Boolean productFound = false;
+                String i = "";
+                
+                while ( !i.equalsIgnoreCase("x") ) {                
+                    
+                    i = input.nextLine();
+                    productFound = false;
+                    if ( i.equalsIgnoreCase("admin") ) {
+                        System.out.print("Please enter the admin password: ");
+                        i = input.nextLine();
+                        System.out.println("Password is invalid");
+                    }
+                    for ( Product p : main.productlist ) {
+                        if( p.getName().equals(i) ) {
+                            if ( p.getQuantity() > 0 ) {
+                                productFound = true;
+                                p.restockProduct( p.getQuantity() - 1 );
+                                System.out.println( "Dispensing... " + p.toString() );
+                            }
+                            else {
+                                System.out.println("That product is out of stock.");
+                            }
+                        }
+                    }
+                
+                    if ( !productFound ) {
+                        System.out.println("Prodduct not in the list.");
+                    }
+                }   
 	}
 
 
