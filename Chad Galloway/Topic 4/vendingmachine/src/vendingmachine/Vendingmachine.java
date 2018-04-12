7fff8262fc90d32169cc2c2014bf75ff48ae4e5c
DESKTOP-A6JNF32\Office Computer <CGalloway3@my.gcu.edu>
2018-04-11 23:22:53 -0500

    More work on the GUI. Added display for money in and purchases cost. Added images to the category buttons.

--- Chad Galloway/Topic 4/vendingmachine/src/vendingmachine/Vendingmachine.java
+++ Chad Galloway/Topic 4/vendingmachine/src/vendingmachine/Vendingmachine.java
@@ -5,6 +5,8 @@
  */
 package vendingmachine;
 
+import java.awt.Color;
+import java.io.FileInputStream;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,12 +14,25 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -27,8 +42,10 @@ import javafx.stage.Stage;
 public class Vendingmachine extends Application {
     
     private Boolean adminMode = false;  // adminMode flag
+    private int moneyInserted = 0;
+    private int productsCost = 0;
     
-    // GUI element declarations
+    // GUI node declarations
     private StackPane customerPane = new StackPane();
     private BorderPane customerBorder = new BorderPane();
     private HBox largeCoinSlot = new HBox();
@@ -48,13 +65,22 @@ public class Vendingmachine extends Application {
     private Button btnAddQuater = new Button("$.25");
     private Button btnAddDime = new Button("$.10");
     private Button btnAddNickle = new Button("$.05");
-    private Label lblBlank = new Label("  ");
+    private Label lblLeftBlank = new Label("  ");
+    private Label lblRightBlank = new Label("  ");
     private Button btnNextPage = new Button("Next");
     private Button btnPreviousPage = new Button("Previous");
     private Button btnDrinkCategory = new Button("Drinks");
+    private final Image imgDrinkCategory = new Image(getClass().getResourceAsStream("images/drinkCategory.jpg"));
+    private final ImageView viewDrinkCategory = new ImageView(imgDrinkCategory);
     private Button btnChipsCategory = new Button("Chips");
+    private final Image imgChipsCategory = new Image(getClass().getResourceAsStream("images/chipsCategory.jpg"));
+    private final ImageView viewChipsCategory = new ImageView(imgChipsCategory);
     private Button btnCandyCategory = new Button("Candy");
+    private final Image imgCandyCategory = new Image(getClass().getResourceAsStream("images/candyCategory.jpg"));
+    private final ImageView viewCandyCategory = new ImageView(imgCandyCategory);
     private Button btnGumCategory = new Button("Gum");
+    private final Image imgGumCategory = new Image(getClass().getResourceAsStream("images/gumCategory.jpeg"));
+    private final ImageView viewGumCategory = new ImageView(imgGumCategory);
     private Button btnItem1_1 = new Button();
     private Button btnItem1_2 = new Button();
     private Button btnItem1_3 = new Button();
@@ -64,6 +90,10 @@ public class Vendingmachine extends Application {
     private Button btnItem3_1 = new Button();
     private Button btnItem3_2 = new Button();
     private Button btnItem3_3 = new Button();
+    private Label lblFunds = new Label("Funds:");
+    private Text txtFundsAmount = new Text("$0.00");
+    private Label lblCost = new Label("Cost:");
+    private Text txtCostAmount = new Text("$0.00");
     private Button btnReturnMoney = new Button("Coin Return");
     private Button btnCompletePurchase = new Button("Complete Purchase");
     private Button btnFinished = new Button("Finished");
@@ -93,21 +123,37 @@ public class Vendingmachine extends Application {
         btnPreviousPage.setDisable(true);
         
         btnDrinkCategory.setPrefSize(100, 100);
+        viewDrinkCategory.setFitHeight(100);
+        viewDrinkCategory.setFitWidth(100);
+        btnDrinkCategory.setContentDisplay(ContentDisplay.TOP);
+        btnDrinkCategory.setGraphic(viewDrinkCategory);
         btnDrinkCategory.setOnAction((event) -> {
             populateItemGrid(1,1);
         });
         
         btnChipsCategory.setPrefSize(100, 100);
+        viewChipsCategory.setFitHeight(100);
+        viewChipsCategory.setFitWidth(100);
+        btnChipsCategory.setContentDisplay(ContentDisplay.TOP);
+        btnChipsCategory.setGraphic(viewChipsCategory);
         btnChipsCategory.setOnAction((event) -> {
             populateItemGrid(2,1);
         });
         
         btnCandyCategory.setPrefSize(100, 100);
+        viewCandyCategory.setFitHeight(100);
+        viewCandyCategory.setFitWidth(100);
+        btnCandyCategory.setContentDisplay(ContentDisplay.TOP);
+        btnCandyCategory.setGraphic(viewCandyCategory);
         btnCandyCategory.setOnAction((event) -> {
             populateItemGrid(3,1);
         });
         
         btnGumCategory.setPrefSize(100, 100);
+        viewGumCategory.setFitHeight(100);
+        viewGumCategory.setFitWidth(100);
+        btnGumCategory.setContentDisplay(ContentDisplay.TOP);
+        btnGumCategory.setGraphic(viewGumCategory);
         btnGumCategory.setOnAction((event) -> {
             populateItemGrid(4,1);
         });
@@ -122,9 +168,55 @@ public class Vendingmachine extends Application {
         btnItem3_2.setPrefSize(70, 70);
         btnItem3_3.setPrefSize(70, 70);
         
-        btnFinished.setOnAction((ActionEvent event) -> {
+        btnFinished.setOnAction((event) -> {
             customerStage.hide();
+            customerBorder.setCenter(categoryGrid);
             primaryStage.show();
+        });
+        
+        btnAddDime.setOnAction((event) -> {
+            moneyInserted += 10;
+            updateFunds();
+        });
+        
+        btnAddFiftyCentCoin.setOnAction((event) -> {
+            moneyInserted += 50;
+            updateFunds();
+        });
+        
+        btnAddFiveDollars.setOnAction((event) -> {
+            moneyInserted += 500;
+            updateFunds();
+        });
+        
+        btnAddNickle.setOnAction((event) -> {
+            moneyInserted += 5;
+            updateFunds();
+        });
+        
+        btnAddOneDollar.setOnAction((event) -> {
+            moneyInserted += 100;
+            updateFunds();
+        });
+        
+        btnAddOneDollarCoin.setOnAction((event) -> {
+            moneyInserted += 100;
+            updateFunds();
+        });
+        
+        btnAddQuater.setOnAction((event) -> {
+            moneyInserted += 25;
+            updateFunds();
+        });
+        
+        btnAddTenDollars.setOnAction((event) -> {
+            moneyInserted += 1000;
+            updateFunds();
+        });
+        
+        btnAddTwentyDollars.setOnAction((event) -> {
+            moneyInserted += 2000;
+            updateFunds();
         });
         
         btnAddOneDollar.prefWidthProperty().bind(smallCoinSlot.widthProperty());
@@ -132,17 +224,29 @@ public class Vendingmachine extends Application {
         btnAddTenDollars.prefWidthProperty().bind(smallCoinSlot.widthProperty());
         btnAddTwentyDollars.prefWidthProperty().bind(smallCoinSlot.widthProperty());
         
+        btnReturnMoney.prefWidthProperty().bind(btnCompletePurchase.widthProperty());
+        btnFinished.prefWidthProperty().bind(btnCompletePurchase.widthProperty());
+
         // Build left section of border pane with coin and money slots
         largeCoinSlot.getChildren().addAll(btnAddOneDollarCoin, btnAddFiftyCentCoin);
         largeCoinSlot.setAlignment(Pos.CENTER);
         smallCoinSlot.getChildren().addAll(btnAddQuater, btnAddDime, btnAddNickle);
         smallCoinSlot.setAlignment(Pos.CENTER);
-        moneySlot.getChildren().addAll(lblCoins, largeCoinSlot, smallCoinSlot, lblBlank, lblBills, btnAddOneDollar, btnAddFiveDollars, btnAddTenDollars, btnAddTwentyDollars);
+        moneySlot.getChildren().addAll(lblCoins, largeCoinSlot, smallCoinSlot, lblLeftBlank, lblBills, btnAddOneDollar, btnAddFiveDollars, btnAddTenDollars, btnAddTwentyDollars);
         moneySlot.setAlignment(Pos.CENTER);
         customerBorder.setLeft(moneySlot);
        
         // Build Right section of border pane with customer controls
-        customerControls.getChildren().addAll(btnCompletePurchase, btnReturnMoney, btnFinished);
+        lblFunds.setMinHeight(20);
+        txtFundsAmount.setFont(Font.font("courier", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        txtFundsAmount.setFill(Paint.valueOf("Black"));
+        txtFundsAmount.setStroke(Paint.valueOf("Green"));
+        lblCost.setMinHeight(20);
+        txtCostAmount.setFont(Font.font("courier", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        txtCostAmount.setFill(Paint.valueOf("Black"));
+        txtCostAmount.setStroke(Paint.valueOf("Red"));
+        
+        customerControls.getChildren().addAll(lblFunds, txtFundsAmount, lblCost, txtCostAmount, lblRightBlank, btnCompletePurchase, btnReturnMoney, btnFinished);
         customerControls.setAlignment(Pos.BOTTOM_CENTER);
         customerBorder.setRight(customerControls);
         
@@ -224,8 +328,17 @@ public class Vendingmachine extends Application {
         launch(args);
     }
 
+    /**
+     *  @param category product category 1=drink 2=chips 3=candy 4=gum 
+     *  @param page page number to display 1=first page 2=second...
+     */
     private void populateItemGrid(int category, int page) {
         customerBorder.setCenter(itemGrid);
+    }
+
+    private void updateFunds() {
+        String text = String.format("$" + moneyInserted / 100 + ".%02d", moneyInserted % 100);
+        txtFundsAmount.setText(text);
     }
 
  }
