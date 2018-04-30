c36fd41ad74d75fd77ee1566b4c441a6903fdc33
DESKTOP-A6JNF32\Office Computer <CGalloway3@my.gcu.edu>
2018-04-29 22:19:26 -0500

    Changes to Quicksort for arraylists

--- Joint submission/Topic 6/vendingmachine/src/vendingmachine/Quicksort.java
+++ Joint submission/Topic 6/vendingmachine/src/vendingmachine/Quicksort.java
@@ -7,22 +7,25 @@
  **/
 package vendingmachine;
 
-import static sun.text.normalizer.UTF16.charAt;
+import java.util.ArrayList;
+import sun.text.normalizer.UTF16;
+
+
 
 public class Quicksort {
-    private static void sort(String[] a) {
-        sort(a, 0, a.length - 1, 0);
+    private static void sortAlpha(ArrayList<InventoryManager.InventoryItem> a) {
+        sort(a, 0, a.size() - 1, 0);
     }
 
-    private static void sort(String[] a, int low, int high, int d)
+    private static void sort(ArrayList<InventoryManager.InventoryItem> a, int low, int high, int d)
     {
         if (high <= low) return;
         int lt = low, gt = high;
-        int v = charAt(a[low], d);
+        int v = UTF16.charAt(a.get(low).getName(), d);
         int i = low + 1;
         while (i <= gt)
         {
-            int t = charAt(a[i], d);
+            int t = UTF16.charAt(a.get(i).getName(), d);
             if (t < v) exchange(a, lt++, i++);
             else if (t > v) exchange(a, i, gt--);
             else
@@ -34,7 +37,9 @@ public class Quicksort {
         if(v >= 0) sort(a, lt, gt, d+1);
         sort(a, gt+1, high, d);
     }
- private static void exchange(String[] a, int i, int i1) {
-
+ private static void exchange(ArrayList<InventoryManager.InventoryItem> a, int i, int i1) {
+     InventoryManager.InventoryItem temp = a.get(i);
+     a.add(i, a.get(i1));
+     a.add(i1, temp);
  }
 }
